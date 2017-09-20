50dc31d7e362519424b6fb3beb0f393f0f3488c0
David <da70@nyu.edu>
2017-09-20 12:47:38 -0400

    enm-pages-browser: enhance topics display in results

--- dev/enm-pages-browser/js/widgets/enm-pages-browser/ResultWidget.js
+++ dev/enm-pages-browser/js/widgets/enm-pages-browser/ResultWidget.js
@@ -92,7 +92,7 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
     var output = '<article><h1 class="title">' + doc.title + ' (page ' + doc.pageNumberForDisplay + ')</h1>';
     output += '<div class="authors"><span class="infolabel">Authors: </span> ' + doc.authors + '</div>';
     output += '<div class="isbn"><span class="infolabel">ISBN: </span>' + doc.isbn + '</div>';
-    output += '<ul class="topics" id="topics_' + doc.id + '"></ul>';
+    output += '<div class="topics"><span class="infolabel">Topics: </span><ul class="topicslist" id="topics_' + doc.id + '"></ul></div>';
     output += '<div class="fulltext">' + snippet + '</div></article>';
     return output;
   },
