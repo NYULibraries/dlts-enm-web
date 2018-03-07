4db01c0df73c5ace22498ce06af8120a47a33001
David <da70@nyu.edu>
2018-03-06 21:52:38 -0500

    prototypes/search-results/js/search-results.js BUGFIX: change discovery to discovery1

--- prototypes/search-results/js/search-results.js
+++ prototypes/search-results/js/search-results.js
@@ -5,7 +5,7 @@ var ALTERNATE_NAMES_LIST_SEPARATOR = '&nbsp;&bull;&nbsp;',
     SOLR_SERVER = {
         'devweb1.dlib.nyu.edu' : 'dev-discovery.dlib.nyu.edu',
         'stageweb1.dlib.nyu.edu' : 'stagediscovery.dlib.nyu.edu',
-        'web1.dlib.nyu.edu' : 'discovery.dlib.nyu.edu'
+        'web1.dlib.nyu.edu' : 'discovery1.dlib.nyu.edu'
     },
 
     // Use appropriate Solr server.  If web host is unknown (ex. localhost), then
